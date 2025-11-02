9bb41b70782d6e30297051a671d7fc3ce524423a
Luiz Moura <lcmoura@yahoo.com>
2025-11-02 11:16:50 -0300

    Removendo arquivos deletados localmente

--- C/hello.c
+++
@@ -1,7 +0,0 @@
-#gcc font.c |-o exe|	em caso de omissão, a.out é gerado automaticamente
-#./exe
-#include <stdio.h>
-int main(){
-	printf("Hello, world!\n");
-	return 0;
-}
--- Node/hello1.js
+++ Node/hello1.js
@@ -1,2 +1,12 @@
 /* Hello World! program in Node.js */
 console.log("Oi, mundo!")
+
+x = "The answer is " + 42; // "The answer is 42"
+console.log(x)
+y = 42 + " is the answer"; // "42 is the answer"
+console.log(y)
+z = "37" + 7; // "377"
+console.log(z)
+
+console.log("37" - 7); // 30
+console.log("37" * 7); // 259
